9d9d19ce83770d8abf4c0af2f814085a41dd20d2
yimagudao <lianfumou@hotmail.com>
2018-11-05 21:33:22 +0800

    create problem6

--- Problem5/src/ReplaceSpaces.java
+++ Problem5/src/ReplaceSpaces.java
@@ -3,28 +3,28 @@
  * @date 2018/11/5 17:21
  */
 public class ReplaceSpaces {
-    public static void ReplaceSpaces(char[] string,int length){
-        if (string == null || length <= 0){
+    public static void ReplaceSpaces(char[] string, int length) {
+        if (string == null || length <= 0) {
             return;
         }
         int numberOfBlank = 0;
-        for (int i = 0; i < length;++i){
+        for (int i = 0; i < length; ++i) {
             if (string[i] == ' ') {
                 numberOfBlank++;
             }
         }
         int newLength = length + 2 * numberOfBlank;
-        if (newLength > string.length){
+        if (newLength > string.length) {
             return;
         }
         int indexOfOriginal = length - 1;
         int indexOfNew = newLength - 1;
-        while (indexOfOriginal >= 0 && indexOfNew > indexOfOriginal){
-            if (string[indexOfOriginal] == ' '){
+        while (indexOfOriginal >= 0 && indexOfNew > indexOfOriginal) {
+            if (string[indexOfOriginal] == ' ') {
                 string[indexOfNew--] = '0';
                 string[indexOfNew--] = '2';
                 string[indexOfNew--] = '%';
-            }else {
+            } else {
                 string[indexOfNew--] = string[indexOfOriginal];
             }
             indexOfOriginal--;
@@ -35,10 +35,10 @@ public class ReplaceSpaces {
         String str = "we are happy.";
         char[] string = new char[30];
         char[] str2 = str.toCharArray();
-        for (int i = 0; i < str2.length; i++){
+        for (int i = 0; i < str2.length; i++) {
             string[i] = str2[i];
         }
-        ReplaceSpaces(string,str2.length);
+        ReplaceSpaces(string, str2.length);
         System.out.println(string);
     }
 }
---
+++ Problem6/src/PrintListReversedOrder.java
@@ -0,0 +1,47 @@
+import java.util.Stack;
+
+/**
+ * @author William Mou
+ * @date 2018/11/5 21:05
+ */
+public class PrintListReversedOrder {
+    //栈
+    public static void PrintListReversingly_Iteratively(ListNode pNode){
+        Stack<ListNode> nodes = new Stack<>();
+        while (pNode != null){
+            nodes.push(pNode);
+            pNode = pNode.next;
+        }
+        while (!nodes.isEmpty()){
+            System.out.println(nodes.pop().value);
+        }
+    }
+    //递归
+    public static void PrintListReversing_Recursively(ListNode pHead){
+        if (pHead == null){
+            return;
+        }
+        PrintListReversing_Recursively(pHead.next);
+        System.out.println(pHead.value);
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode();
+        ListNode node2 = new ListNode();
+        ListNode node3 = new ListNode();
+        node1.value = 3;
+        node2.value = 2;
+        node3.value = 1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = null;
+        PrintListReversingly_Iteratively(node1);
+        PrintListReversing_Recursively(node1);
+
+    }
+}
+
+class ListNode{
+    int value;
+    ListNode next;
+}
